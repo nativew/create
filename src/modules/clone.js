8a8f815ac56087f6916e6467c302df2088901aac
Antoine Boulanger <hello@antoineboulanger.com>
2020-05-12 12:52:09 -0400

    Add clone depth option, Replace experimental del with run rm

--- src/modules/clone.js
+++ src/modules/clone.js
@@ -1,12 +1,12 @@
 const { url } = require('../data');
 const run = require('./run');
-const del = require('./del');
 
 function clone(dest) {
-	const cmd = run('git', ['clone', url, dest]);
+	const cmd = run('git', ['clone', '--depth=1', url, dest]);
 
 	if (cmd.status == 0) {
-		del(`${dest}/.git`);
+		// Delete .git folder
+		run('rm', ['-rf', `${dest}/.git`]);
 		return true;
 	} else {
 		return false;
--- src/modules/del.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-function del(path) {
-	fs.rmdirSync(path, { recursive: true });
-}
-
-module.exports = del;
